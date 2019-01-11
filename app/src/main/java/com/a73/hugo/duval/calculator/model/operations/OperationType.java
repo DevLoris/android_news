d6f0e6f8f9f40f99cd2f516bb9c0d0e8e6785047
Digital <digital@c-23.local>
2019-01-11 13:43:12 +0100

    Update ignore

--- app/src/main/java/com/a73/hugo/duval/calculator/model/operations/OperationType.java
+++
@@ -1,8 +0,0 @@
-package com.a73.hugo.duval.calculator.model.operations;
-
-public final class OperationType {
-    public static final String ADD      = "OPERATION_ADD";
-    public static final String SUBTRACT = "OPERATION_SUBTRACT";
-    public static final String MULTIPLY = "OPERATION_MULTIPLY";
-    public static final String DIVIDE   = "OPERATION_DIVIDE";
-}
